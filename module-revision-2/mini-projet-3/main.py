ae22cf87aa9ce83c504dcb4197c8ce82dc8f6f45
Oussecg <anasoussema16@gmail.com>
2025-09-24 12:36:18 +0100

    We finish mini-project-3

---
+++ module-revision-2/mini-projet-3/main.py
@@ -0,0 +1,20 @@
+def saisir_texte():
+    ch = input("Saisir un texte: ")
+    while not "A" <= ch[0].upper() <= "Z":
+        ch = input("Retaper un texte: ")
+    return ch
+
+def supprimer_espace(ch: str):
+    if (ch.find(" ") != -1):
+        ch1 = ""
+        i = 0
+        while i < len(ch) - 1:
+            if not(ch[i] == " " and ch[i + 1] == " "):
+                ch1 += ch[i]
+            i += 1
+    else:
+        ch1 = ch
+    return ch1
+
+ch = saisir_texte()
+print(supprimer_espace(ch))
