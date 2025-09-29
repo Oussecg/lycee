59f0cf45c5c2655ae103dbe3eff37035202f59e7
Oussecg <anasoussema16@gmail.com>
2025-09-29 19:25:34 +0100

    We making some good progress on the mini-project-4

--- mini-project-4/main.py
+++ mini-project-4/main.py
@@ -1,7 +1,5 @@
 def saisir():
     ch = input("Ch= ")
-    while not est_formule(ch):
-        ch = input("Ch= ")
     return ch
 
 def est_formule(ch: str):
@@ -10,20 +8,28 @@ def est_formule(ch: str):
     return False
 
 def calculer(ch: str):
-    math_operations = ["*", "+", "/", "-"]
+    equation_list = []
     r = 0
     i = 0
     while i < len(ch):
         if ch[i] in math_operations:
-            if ch[i] == "+":
-                r += int(ch[: i])
+            print(ch[:i], ch[i], ch[i + 1: ])
+            equation_list.append(int(ch[:i]))
+            equation_list.append(ch[i])
+            if check_math(ch[i + 1: ]):
+                equation_list.append(ch[i])
+            else:
                 ch = ch[i + 1: ]
-                i = 0
-        elif (ch[i] in math_operations) == False and i + 1 == len(ch) :
-            r += int(ch)
-        print(f"Ch = {ch} and ch[{i}] = {ch[i]} and r = {r}")
+            i = 0
+            print(equation_list)
         i += 1
     return r
 
+def check_math(ch):
+    for c in ch:
+        if c in math_operations:
+            return False
+    return True
+math_operations = ["*", "+", "/", "-"]
 ch = saisir()
 calculer(ch)
