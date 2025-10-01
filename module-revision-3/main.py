a62656b1a0d4408d2dbf2b80e34fe3a79f0b2e11
Oussecg <anasoussema16@gmail.com>
2025-10-01 09:18:25 +0100

    Module de revision 3

---
+++ module-revision-3/main.py
@@ -0,0 +1,28 @@
+from numpy import array
+
+def nombre_eleve():
+    x = int(input("Donner le nombre d'élèves: "))
+    while not (3 < x <= 25):
+        x = int(input("Donner le nombre d'élèves: "))
+    return x
+
+def remplir_tableau(t, N):
+    for i in range(N):
+        t[i] = float(input("Moyenne de l'élève", i + 1, ":"))
+        while not (0 <= t[i] <= 20):
+            t[i] = float(input("Moyenne de l'élève", i + 1, ":"))
+    return t
+
+def moyen_generale(t, n):
+    mg = 0
+    for i in range(n):
+        mg += t[i]
+    mg = round(mg/n, 2)
+    return mg
+        
+N = nombre_eleve()
+t = array([int] * N)
+Mg = moyen_generale(t, N)
+# Ne = nombre_superieur(t, N, Mg)
+# My = max_moyen(t, N)
+print("Le moyen generale est:", Mg)
